0f55622b48cceb34b00ce59c64b96efe53a6c62c
NestorSo <NestorSoto2601@gmail.com>
2023-09-16 16:18:07 -0600

    Acceso a datos con ADO.Net

---
+++ NorthwindEF/Models/NorthwindContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindEF.Models;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["constring"].ConnectionString);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasIndex(e => e.City, "City");
+
+            entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.HasIndex(e => e.Region, "Region");
+
+            entity.Property(e => e.CustomerId)
+                .HasMaxLength(5)
+                .IsFixedLength()
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.ContactName).HasMaxLength(30);
+            entity.Property(e => e.ContactTitle).HasMaxLength(30);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Fax).HasMaxLength(24);
+            entity.Property(e => e.Phone).HasMaxLength(24);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasIndex(e => e.LastName, "LastName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Extension).HasMaxLength(4);
+            entity.Property(e => e.FirstName).HasMaxLength(10);
+            entity.Property(e => e.HireDate).HasColumnType("datetime");
+            entity.Property(e => e.HomePhone).HasMaxLength(24);
+            entity.Property(e => e.LastName).HasMaxLength(20);
+            entity.Property(e => e.Photo).HasColumnType("image");
+            entity.Property(e => e.PhotoPath).HasMaxLength(255);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+            entity.Property(e => e.Title).HasMaxLength(30);
+            entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
+
+            entity.HasOne(d => d.ReportsToNavigation).WithMany(p => p.InverseReportsToNavigation)
+                .HasForeignKey(d => d.ReportsTo)
+                .HasConstraintName("FK_Employees_Employees");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ winSQLClient/frmProducts.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winSQLClient
+{
+    public partial class frmProducts : Form
+    {
+        //Conexión global a SQL Server
+        string connectionString = ConfigurationManager.ConnectionStrings["winSQLClient.Properties.Settings.Con"].ConnectionString;
+
+        public frmProducts()
+        {
+            InitializeComponent();
+            GetProducts();
+        }
+
+        private void GetProducts()
+        {
+            string queryString = "SELECT ProductID, UnitPrice, ProductName " +
+    "FROM Products ORDER BY UnitPrice DESC;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dgvProducts.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvProducts.Rows[e.RowIndex];
+                txtProductID.Text = row.Cells["ProductID"].Value.ToString();
+                txtProductName.Text = row.Cells["ProductName"].Value.ToString();
+                txtUnitPrice.Text = row.Cells["UnitPrice"].Value.ToString();
+            }
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            InsertProducts();
+        }
+
+        private void InsertProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string queryString = "INSERT INTO Products(ProductName,UnitPrice) VALUES('" + txtProductName.Text
+                        + "', " + txtUnitPrice.Text + ")";
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto guardado correctamente");
+                    GetProducts();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            UpdateProducts();
+        }
+
+        private void UpdateProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string queryString = "UPDATE Products SET ProductName = '" + txtProductName.Text
+                        + "', UnitPrice = " + txtUnitPrice.Text + " WHERE ProductID = " + txtProductID.Text;
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto actualizado correctamente");
+                    GetProducts();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            DeleteProducts();
+        }
+
+        private void DeleteProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string queryString = "DELETE FROM Products WHERE ProductId = " + txtProductID.Text;
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto eliminado correctamente");
+                    GetProducts();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
